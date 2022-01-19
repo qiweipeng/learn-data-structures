d845edbab8980a2a9f6049fc74e66b2365c55d2c
Weipeng Qi <qiweipeng@hotmail.com>
2022-01-19 11:19:11 +0800

    Add settings.json

--- src/App.java
+++ src/App.java
@@ -1,13 +1,13 @@
 /*
  * @Author: Weipeng Qi
  * @Date: 2022-01-18 21:13:10
- * @LastEditTime: 2022-01-18 21:16:24
+ * @LastEditTime: 2022-01-19 11:16:57
  * @FilePath: /learn-data-structures/src/App.java
  * Copyright © 2022 Weipeng Qi. All rights reserved.
  */
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
